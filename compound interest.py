474528f202842ecb28cd4bda2bba3ccbbb6217f3
PAVANKUMAR B <67587214+pavankumar003b@users.noreply.github.com>
2020-07-01 18:24:27 +0530

    Add files via upload

---
+++ compound interest.py
@@ -0,0 +1,9 @@
+#compound interest
+def compound_interest(p,r,t):
+    print('the principle is',p)
+    print('the rate is',r)
+    print('the time is',t)
+    ci=p*(pow((1+r/100),t))
+    print('compound interest is',ci)
+    return ci
+compound_interest(100, 3.5, 4)
