13b8b529ecfdaccbe6d3f9a616907bf54cd8ae53
PAVANKUMAR B <67587214+pavankumar003b@users.noreply.github.com>
2020-07-04 19:06:12 +0530

    Add files via upload

---
+++ program-to-find-sum-of-array.py
@@ -0,0 +1,10 @@
+def _sum(arr,n):
+    return(sum(arr))               #return sum by using sum
+
+arr=[]                      
+arr = [10,4,3,12]                  # input values to list 
+
+n = len(arr)                       # calculate length of array 
+ans = _sum(arr,n)
+
+print('sum of the array is',ans)   # display sum 
